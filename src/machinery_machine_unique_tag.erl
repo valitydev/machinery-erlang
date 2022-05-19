a7e69abbf3f9e7edbb232644e727ee2ccba398fd
kehitt <kehitt@users.noreply.github.com>
2022-05-19 14:07:33 +0300

    restore some deleted files

---
+++ src/machinery_machine_unique_tag.erl
@@ -0,0 +1,143 @@
+%%%
+%%% Machine behaving as a unique tag.
+
+-module(machinery_machine_unique_tag).
+
+%% API
+
+-type id() :: machinery:id().
+-type namespace() :: machinery:namespace().
+-type timer() :: machinery:timer().
+-type tag() :: binary().
+
+-export_type([tag/0]).
+
+-export([tag/4]).
+-export([tag_until/5]).
+-export([untag/4]).
+-export([get/3]).
+
+%% Machine behaviour
+
+-behaviour(machinery).
+
+-export([init/4]).
+-export([process_timeout/3]).
+-export([process_repair/4]).
+-export([process_call/4]).
+
+%%
+
+-spec tag(namespace(), tag(), id(), machinery:backend(_)) -> ok | {error, {set, id()}}.
+tag(NS, Tag, ID, Backend) ->
+    case machinery:start(construct_namespace(NS), Tag, {tag, ID}, Backend) of
+        ok ->
+            ok;
+        {error, exists} ->
+            case get(NS, Tag, Backend) of
+                {ok, ID} ->
+                    ok;
+                {ok, IDWas} ->
+                    {error, {set, IDWas}};
+                {error, unset} ->
+                    tag(NS, Tag, ID, Backend)
+            end
+    end.
+
+-spec tag_until(namespace(), tag(), id(), timer(), machinery:backend(_)) -> ok | {error, {set, id()}}.
+tag_until(NS, Tag, ID, Timer, Backend) ->
+    case machinery:start(construct_namespace(NS), Tag, {tag, ID, Timer}, Backend) of
+        ok ->
+            ok;
+        {error, exists} ->
+            case get(NS, Tag, Backend) of
+                {ok, ID} ->
+                    ok;
+                {ok, IDWas} ->
+                    {error, {set, IDWas}};
+                {error, unset} ->
+                    tag_until(NS, Tag, ID, Timer, Backend)
+            end
+    end.
+
+-spec untag(namespace(), tag(), id(), machinery:backend(_)) -> ok | {error, {set, id()}}.
+untag(NS, Tag, ID, Backend) ->
+    case machinery:call(construct_namespace(NS), Tag, {untag, ID}, Backend) of
+        {ok, ok} ->
+            ok;
+        {ok, {error, IDWas}} ->
+            {error, {set, IDWas}};
+        {error, notfound} ->
+            ok
+    end.
+
+-spec get(namespace(), tag(), machinery:backend(_)) -> {ok, id()} | {error, unset}.
+get(NS, Tag, Backend) ->
+    case machinery:get(construct_namespace(NS), Tag, Backend) of
+        {ok, Machine} ->
+            ID = get_machine_st(Machine),
+            {ok, ID};
+        {error, notfound} ->
+            {error, unset}
+    end.
+
+construct_namespace(NS) ->
+    list_to_atom(atom_to_list(NS) ++ "/tags").
+
+%%
+
+-type machine() :: machinery:machine(ev(), _).
+-type handler_opts() :: machinery:handler_opts(_).
+-type result() :: machinery:result(ev(), _).
+-type response() ::
+    machinery:response(
+        ok | {error, id()}
+    ).
+
+-type ev() :: id().
+
+-spec init({tag, id()}, machine(), undefined, handler_opts()) -> result().
+init({tag, ID}, _Machine, _, _Opts) ->
+    #{
+        events => [ID]
+    };
+init({tag, ID, Timer}, _Machine, _, _Opts) ->
+    #{
+        events => [ID],
+        action => [{set_timer, Timer, {undefined, 0, forward}}]
+    }.
+
+-spec process_timeout(machine(), undefined, handler_opts()) -> result().
+process_timeout(#{}, _, _Opts) ->
+    #{
+        action => [remove]
+    }.
+
+-spec process_call({untag, id()}, machine(), undefined, handler_opts()) -> {response(), result()}.
+process_call({untag, ID}, Machine, _, _Opts) ->
+    case get_machine_st(Machine) of
+        ID ->
+            {ok, #{
+                action => [remove]
+            }};
+        IDWas ->
+            {{error, IDWas}, #{}}
+    end.
+
+-spec process_repair({untag, id()}, machine(), undefined, handler_opts()) -> no_return().
+process_repair(_Args, _Machine, _, _Opts) ->
+    erlang:error({not_implemented, repair}).
+
+%%
+
+get_machine_st(#{history := History}) ->
+    collapse_history(History).
+
+collapse_history(History) ->
+    lists:foldl(fun apply_event/2, undefined, History).
+
+apply_event({_ID, _CreatedAt, Body}, St) ->
+    apply_event_body(Body, St).
+
+apply_event_body(ID, undefined) ->
+    ID.
---
+++ src/machinery_machine_unique_tag_mg_example.erl
@@ -0,0 +1,90 @@
+-module(machinery_machine_unique_tag_mg_example).
+
+%% API
+
+-export([child_spec/1]).
+-export([tag/4]).
+-export([tag_until/5]).
+-export([untag/4]).
+-export([get/3]).
+
+-type id() :: machinery:id().
+-type namespace() :: machinery:namespace().
+-type tag() :: machinery_machine_unique_tag:tag().
+-type timer() :: machinery:timer().
+
+-type opts() :: #{
+    woody_ctx := woody_context:ctx()
+}.
+
+-export_type([opts/0]).
+
+-define(LOGIC_HANDLER, machinery_machine_unique_tag).
+
+%% API
+-spec child_spec(_Id) -> supervisor:child_spec().
+child_spec(Id) ->
+    machinery_utils:woody_child_spec(Id, get_routes(), config(server_opts)).
+
+-spec tag(namespace(), tag(), id(), opts()) -> ok | {error, {set, id()}}.
+tag(NS, Tag, ID, Opts) ->
+    machinery_machine_unique_tag:tag(NS, Tag, ID, get_backend(Opts)).
+
+-spec tag_until(namespace(), tag(), id(), timer(), opts()) -> ok | {error, {set, id()}}.
+tag_until(NS, Tag, ID, Timer, Opts) ->
+    machinery_machine_unique_tag:tag_until(NS, Tag, ID, Timer, get_backend(Opts)).
+
+-spec untag(namespace(), tag(), id(), opts()) -> ok | {error, {set, id()}}.
+untag(NS, Tag, ID, Opts) ->
+    machinery_machine_unique_tag:untag(NS, Tag, ID, get_backend(Opts)).
+
+-spec get(namespace(), tag(), opts()) -> {ok, id()} | {error, unset}.
+get(NS, Tag, Opts) ->
+    machinery_machine_unique_tag:get(NS, Tag, get_backend(Opts)).
+
+%%% Internal functions
+
+%% handler
+-spec get_routes() -> machinery_utils:woody_routes().
+get_routes() ->
+    machinery_mg_backend:get_routes(
+        [get_handler(config(handler_path))],
+        maps:with([event_handler, handler_limits], config())
+    ).
+
+get_handler(Path) ->
+    {?LOGIC_HANDLER, get_handler_config(Path)}.
+
+get_handler_config(Path) ->
+    #{path => Path, backend_config => get_backend_config()}.
+
+get_backend_config() ->
+    maps:with([schema], config()).
+
+%% client
+-spec get_backend(opts()) -> machinery_mg_backend:backend().
+get_backend(#{woody_ctx := WoodyCtx}) ->
+    machinery_mg_backend:new(WoodyCtx, #{
+        client => get_woody_client(),
+        schema => config(schema)
+    }).
+
+-spec get_woody_client() -> machinery_mg_client:woody_client().
+get_woody_client() ->
+    maps:with([url, event_handler], config()).
+
+%% config
+config(Key) ->
+    maps:get(Key, config()).
+
+config() ->
+    #{
+        schema => machinery_mg_schema_generic,
+        url => <<"http://machinegun:8022/v1/automaton">>,
+        handler_path => <<"/v1/stateproc">>,
+        event_handler => woody_event_handler_default,
+        server_opts => #{
+            ip => {0, 0, 0, 0},
+            port => 8022
+        }
+    }.
